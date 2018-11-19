0f7f3ea099dcc4149e511d540fb18e8109fdd6f9
HaoRan Yu <eudemonrandy@hotmail.com>
2018-11-19 17:51:33 -0500

    added layout, controllers and database migration

---
+++ database/migrations/2018_11_19_223351_init.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Init extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+            $table->string('format');
+            $table->integer('length');
+            $table->integer('year');
+            $table->tinyInteger('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('movie');
+    }
+}
